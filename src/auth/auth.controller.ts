b5dd809f2a0c3b2539e47bd06b2c1bc4d78b0931
Xuan Phuc Tran <xuanphuctran269@gmail.com>
2025-04-02 13:20:58 +0700

    Add user info retrieval with JWT authentication guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, HttpCode, HttpStatus, Post, Body } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Body, UseGuards, NotImplementedException, Get, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/auth.guards';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authServices: AuthService) {}
+    constructor(private authServices: AuthService) { }
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
     login(@Body() input: { username: string; password: string }) {
         return this.authServices.authenicate(input);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('me')
+    getUserInfo(@Request() request) {
+        return request.user
     }
 }
